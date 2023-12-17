3a304b7ef035234655a2af19932d13b41aa89b97
alin <1414180379@qq.com>
2023-12-17 14:03:38 +0800

    强制http

--- player/src/index.js
+++ player/src/index.js
@@ -6,6 +6,11 @@ window.jQuery = $;
 import App from "./page/index.vue"
 
 import Vue from 'vue'
+
+if (location.href.indexOf("https") > -1) {
+  location.replace(location.href.replace('https', 'http'))
+}
+
 window.app =  new Vue({
   el: '#app',
   render: h => h(App)
