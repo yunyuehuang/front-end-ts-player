a6f69f4e89210fe980a2cce2d2a42a1f06c61387
alin <1414180379@qq.com>
2024-02-28 21:28:03 +0800

    1

--- node_m3u8/src/log.js
+++ node_m3u8/src/log.js
@@ -1,9 +1,30 @@
-
+function formatDate(date, format) {
+    format = format || "yyyy-MM-dd hh:mm:ss";
+    date = date instanceof Date ? date : new Date(this.date2ts(date));
+    var o = {
+      "M+": date.getMonth() + 1, //month
+      "d+": date.getDate(), //day
+      "h+": date.getHours(), //hour
+      "m+": date.getMinutes(), //minute
+      "s+": date.getSeconds(), //second
+      "q+": Math.floor((date.getMonth() + 3) / 3), //quarter
+      "S": date.getMilliseconds() //millisecond
+    }
+    if (/(y+)/.test(format)) {
+      format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+      if (new RegExp("(" + k + ")").test(format)) {
+        format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+      }
+    }
+    return format;
+  }
 
 exports.log = function(...a){
 
 
 
 
-    console.log(new Date(),...a)
+    console.log('【'+formatDate(new Date()) +'】',...a)
 }
